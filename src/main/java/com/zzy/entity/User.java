7829924e8526de2424ff0f4fa63654abf1b386e9
cangshu250 <1550865978@qq.com>
2020-04-23 17:29:27 +0800

    初始化项目

---
+++ src/main/java/com/zzy/entity/User.java
@@ -0,0 +1,42 @@
+package com.zzy.entity;
+
+import java.util.Date;
+
+public class User {
+    private String id;
+    private String name;
+    private int age;
+    private Date birthday;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
